4adde86c754deac3724229f3a81f3675ef8b6a73
Tahim Mondal <133028260+Tahimpro@users.noreply.github.com>
2025-02-23 11:11:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # Configurations for the scraper bot
 
-TELEGRAM_BOT_TOKEN = "your-telegram-bot-token"
-TELEGRAM_CHAT_ID = "your-telegram-chat-id"
-BASE_URL = "https://www.skymovieshd.bz"
+TELEGRAM_BOT_TOKEN = "7524524705:AAH7aBrV5cAZNRFIx3ZZhO72kbi4tjNd8lI"
+TELEGRAM_CHAT_ID = "-1002340139937"
+BASE_URL = "https://www.skymovieshd.video"
 CHECK_INTERVAL = 180  # 3 minutes (in seconds)
