ba5db6ab485dd009b9cc791724f1a9b585c82512
Tahim Mondal <133028260+Tahimpro@users.noreply.github.com>
2025-02-23 09:01:39 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN", "your_bot_token")
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID", "your_chat_id")
+BASE_URL = "https://skymovieshd.video/"
+CHECK_INTERVAL = 180  # 3 minutes
