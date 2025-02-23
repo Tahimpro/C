160c3078df927a1445936eca1c68e70cb0a9c17c
Tahim Mondal <133028260+Tahimpro@users.noreply.github.com>
2025-02-23 09:21:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN", "your_bot_token")
-TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID", "your_chat_id")
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN", "7524524705:AAH7aBrV5cAZNRFIx3ZZhO72kbi4tjNd8lI")
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID", "-1002340139937")
 BASE_URL = "https://skymovieshd.video/"
 CHECK_INTERVAL = 180  # 3 minutes
